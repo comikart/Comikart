8cce2a5e802203f26f237b42d5e973957920e1ee
ktan114 <ktaneight@gmail.com>
2019-07-17 18:19:45 -0400

    Added proptypes to the Button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import css from './styles.module.scss';
 
 class Button extends Component {
@@ -9,6 +11,7 @@ class Button extends Component {
   render() {
     const style = this.props.style === 'primary' ? css.btnPrimary : '';
     const size = this.props.size === 'large' ? css.btnLarge : '';
+    console.log('props', this.props)
     return (
       <button
         onClick={this.props.onClick}
@@ -18,6 +21,13 @@ class Button extends Component {
       </button>
     );
   }
+}
+
+Button.propTypes = {
+  style: PropTypes.string,
+  size: PropTypes.string,
+  onClick: PropTypes.func,
+  children: PropTypes.string,
 }
 
 export default Button;
